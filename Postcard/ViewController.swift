9ebe0afbe0ab1e1a9833a8b334cd07fa4fa2592f
Seshagiri Vakkalanka <Vakkalanka@Seshagiris-iMac.local>
2014-11-06 10:25:35 -0500

    name label functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -15,8 +15,11 @@ class ViewController: UIViewController {
     //weak is talking about the type of pointer: weak
     //var means variable
     //explanation point is for "optional"...
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    
     @IBOutlet weak var mailButton: UIButton!
     
     
@@ -41,6 +44,13 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text=""
+        enterNameTextField.resignFirstResponder()
         
         
         enterMessageTextField.text = ""
